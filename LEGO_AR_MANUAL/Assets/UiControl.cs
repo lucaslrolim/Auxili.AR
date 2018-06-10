7faa0ba2170dd2a41ccf65a63831f84eb410b458
Lucas Rolim <lucasrolim@poli.ufrj.br>
2018-06-10 00:40:42 -0300

    New assets and buttons

---
+++ LEGO_AR_MANUAL/Assets/UiControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public float scalingspeed = 0.01f;
+	bool scaleUp = false;
+	bool scaleDown = false;
+
+	// Update is called once per frame
+	void Update () {
+		if(scaleUp == true)
+		{
+			scaleUpButton();
+		}
+		if(scaleDown == true){
+			scaleDownButton();
+		}
+	}
+
+	public void scaleUpButton(){
+		GameObject.FindWithTag("Lego").transform.localScale += new Vector3(scalingspeed,scalingspeed,scalingspeed);
+	}
+
+	public void scaleDownButton(){
+		GameObject.FindWithTag("Lego").transform.localScale -= new Vector3(scalingspeed,scalingspeed,scalingspeed);
+	}
+
+	public void up()
+	{
+		scaleUp = true;
+		scaleDown = false;
+	}
+
+	public void down()
+	{
+		scaleDown = true;
+		scaleUp = false;
+	}
+
+	public void stop()
+	{
+		scaleUp = false;
+		scaleDown = false;
+	}
+}

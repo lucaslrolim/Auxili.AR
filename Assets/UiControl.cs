1609efc23cc4786c982a241ba2b192fa6ff27051
Yago Carvalho <yago.carvalho@bigdatacorp.com.br>
2018-06-25 02:13:27 -0300

    Added step-by-step capability

--- Assets/UiControl.cs
+++ Assets/UiControl.cs
@@ -2,58 +2,111 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class UiControl : MonoBehaviour {
-    public GameObject myLego;
-	// Use this for initialization
-	void Start () {
-        myLego = null;
-    }
+using System.Linq;
+using Vuforia;
 
+public class UiControl : MonoBehaviour {
+
+    public Dictionary<string, List<GameObject>> myLegoDict = new Dictionary<string, List<GameObject>>();
 	public float scalingspeed = 0.01f;
-	bool scaleUp = false;
-	bool scaleDown = false;
-	public int step = 1;
+	public bool  scaleUp      = false;
+	public bool  scaleDown    = false;
+	public int   step         = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        step = 0;
+        UpdateStepCounter();
+    }
 
 	// Update is called once per frame
 	void Update () {
 		if(scaleUp == true)
 		{
-			scaleUpButton();
+			ScaleUpButton();
 		}
-		if(scaleDown == true){
-			scaleDownButton();
+
+        if (scaleDown == true)
+        {
+			ScaleDownButton();
 		}
 	}
 
-	public void scaleUpButton(){
-		GameObject.FindWithTag("Lego").transform.localScale += new Vector3(scalingspeed,scalingspeed,scalingspeed);
+	public void ScaleUpButton()
+    {
+		GameObject.FindWithTag("step_0").transform.localScale += new Vector3(scalingspeed,scalingspeed,scalingspeed);
 	}
 
-	public void scaleDownButton(){
-		GameObject.FindWithTag("Lego").transform.localScale -= new Vector3(scalingspeed,scalingspeed,scalingspeed);
+	public void ScaleDownButton()
+    {
+		GameObject.FindWithTag("step_0").transform.localScale -= new Vector3(scalingspeed,scalingspeed,scalingspeed);
 	}
 
-	public void previousStep()
+	public void PreviousStep()
 	{
-        if(step>1)
+        if(step > 0)
         {
             step -= 1;
-            updateStepCounter();
-            myLego.SetActive(false);
+            UpdateStepCounter();
+
+            // Select all GameObjects from next step and set active to false
+            string tag = string.Format("step_{0}", step + 1);
+            print(tag);
+            myLegoDict[tag].ForEach(go => go.SetActive(false));
+
+            // Set view again
+            if (step == 0)
+            {
+               // myLegoDict["step_0"][0].SetActive(true);
+            }
         }
-	}
-	public void nextStep()
-	{
-        if(!myLego)
-        {
-            myLego = GameObject.FindWithTag("Lego");
-        }
-        step += 1;
-        myLego.SetActive(true);
-        updateStepCounter();
 	}
 
-	public void updateStepCounter()
+	public void NextStep()
+	{
+        string tag;
+        if (myLegoDict.Count == 0)
+        {
+            // Initialize all GameObjects as inactive
+            for (int i = 0; i <= 11; i++)
+            {
+                tag = string.Format("step_{0}", i);
+                print("Init: " + tag);
+                foreach (GameObject go in GameObject.FindGameObjectsWithTag(tag).ToList())
+                {
+                    if (!myLegoDict.ContainsKey(tag))
+                    {
+                        myLegoDict.Add(tag, new List<GameObject>());
+                    }
+
+                    myLegoDict[tag].Add(go);
+
+                    go.SetActive(false);
+                }
+            }
+
+            // Enable Full Rocket View
+            //myLegoDict["step_0"][0].SetActive(true);
+        }
+
+        step += 1;
+        UpdateStepCounter();
+
+        // Disable full rocket view
+        if (step > 0)
+        {
+            //myLegoDict["step_0"][0].SetActive(false);
+        }
+
+        // Select all GameObjects from current step and set active to true
+        tag = string.Format("step_{0}", step);
+        print(tag);
+        myLegoDict[tag].ForEach(go => go.SetActive(true));
+
+	}
+
+	public void UpdateStepCounter()
 	{
 		Text textObject = GameObject.FindWithTag("step_counter").GetComponent<Text>();
 		textObject.text = "ETAPA " + step.ToString();
